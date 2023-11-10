13f6f6ef14ba24294ad2547d1273d5a9397ab3e8
Jamshid B. Mohasefi <jbagher52@gmail.com>
2023-11-10 16:20:12 +0330

    file upload works for one file

--- hello-world.js
+++ hello-world.js
@@ -1,26 +1,52 @@
-var http = require('http');
+var http = require('node:http');
 var formidable = require('formidable');
-var fs = require('fs');
+var fs = require ('fs');
 
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.oldpath;
-      console.log(oldpath);
-      var newpath = '/home/anita/tmp/' + files.filetoupload.originalFilename;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
- });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
+
+const server = http.createServer(async (req, res) => {
+  if (req.url === '/upload' && req.method.toLowerCase() === 'post') {
+    // parse a file upload
+    const form = new formidable.IncomingForm();
+    let fields;
+    let files;
+    try {
+        [fields, files] = await form.parse(req);
+    } catch (err) {
+        // example to check for a very specific error
+        if (err.code === formidableErrors.maxFieldsExceeded) {
+
+        }
+        console.error(err);
+        res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
+        res.end(String(err));
+        return;
+    }
+    var oldpath = files.multipleFiles[0].filepath;
+    console.log(oldpath);
+    var newpath = '/home/anita/tmp/' + files.multipleFiles[0].originalFilename;
+    fs.rename(oldpath, newpath, function (err) {
+       if (err) throw err;
+       res.writeHead(200, {'Content-Type': 'text/html'});
+       res.write('<h1> File uploaded! </h1> \n\n');
+       res.end();
+       // res.writeHead(200, { 'Content-Type': 'application/json' });
+       // res.end('JSON.stringify({ fields, files }, null, 2));
+    });
+    return;
   }
-}).listen(8080);
+
+  // show a file upload form
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`
+    <h2>With Node.js <code>"http"</code> module</h2>
+    <form action="upload" enctype="multipart/form-data" method="post">
+      <div>Text field title: <input type="text" name="title" /></div>
+      <div>File: <input type="file" name="multipleFiles" multiple="multiple" /></div>
+      <input type="submit" value="Upload" />
+    </form>
+  `);
+});
+
+server.listen(8080, () => {
+  console.log('Server listening on http://localhost:8080/ ...');
+});
