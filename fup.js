0906a9ed8f243ec7d8520f720e2c3337967a34ac
Jamshid B. Mohasefi <jbagher52@gmail.com>
2023-11-10 16:44:24 +0330

    A complete file upload and file reading program in nodejs

--- fup.js
+++ fup.js
@@ -24,11 +24,15 @@ const server = http.createServer(async (req, res) => {
     var oldpath = files.multipleFiles[0].filepath;
     console.log(oldpath);
     var newpath = '/home/anita/tmp/' + files.multipleFiles[0].originalFilename;
+    
     fs.rename(oldpath, newpath, function (err) {
        if (err) throw err;
-       res.writeHead(200, {'Content-Type': 'text/html'});
-       res.write('<h1> File uploaded! </h1> \n\n');
-       res.end();
+
+       fs.readFile('demofile1.html', function(err, data) {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(data);
+          return res.end();
+       })
        // res.writeHead(200, { 'Content-Type': 'application/json' });
        // res.end('JSON.stringify({ fields, files }, null, 2));
     });
